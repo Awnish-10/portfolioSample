71f21d9cfc3354ecc0e2e3ed799838af5107d09e
Awnish Negi <awnish.negi10@gmail.com>
2021-08-30 19:45:48 +0530

    works updated

---
+++ src/components/WorksData.jsx
@@ -0,0 +1,27 @@
+import DeveloperModeIcon from '@material-ui/icons/DeveloperMode';
+import CodeIcon from '@material-ui/icons/Code';
+import AllInclusiveIcon from '@material-ui/icons/AllInclusive';
+
+export const worksData = [
+    {
+        id: 1,
+        icon: <DeveloperModeIcon />,
+        title: "web Development",
+        about: "Lorem ipsum is placeholder text commonly used in the graphic,print, and publishing industries for previewing layouts and visual mockups.",
+        image: "https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8d2ViJTIwZGV2fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+    },
+    {
+        id: 2,
+        icon: <CodeIcon />,
+        title: "Coding",
+        about: "Lorem ipsum is placeholder text commonly used in the graphic,print, and publishing industries for previewing layouts and visual mockups.",
+        image: "https://images.unsplash.com/photo-1562813733-b31f71025d54?ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8Y29kaW5nfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+    },
+    {
+        id: 3,
+        icon: <AllInclusiveIcon />,
+        title: "Problem Solving",
+        about: "Lorem ipsum is placeholder text commonly used in the graphic,print, and publishing industries for previewing layouts and visual mockups.",
+        image: "https://images.unsplash.com/photo-1539627831859-a911cf04d3cd?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cHJvYmx1bSUyMHNvbHZpbmd8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+    }
+]
--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -2,19 +2,56 @@ import "./works.scss"
 import PhoneAndroidIcon from '@material-ui/icons/PhoneAndroid';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+import {worksData} from "../WorksData.jsx";
+import {useState} from "react";
 
 export default function Works() {
+    const [slide, setSlide] = useState(0);
+
+    function handleClick(direction){
+      direction === "left" ?
+      setSlide(slide == 0 ? 2 : slide -1) ://--slide doesn't work
+      setSlide(slide == 2 ? 0 : slide +1)
+
+    }
     return (
         <div className="works" id="works">
-     
-        <div className="wrapper">
-         <div className="lArrow"><ArrowBackIosIcon classname="MuiSvgIcon-root"/></div>
-        <div className="rArrow"><ArrowForwardIosIcon  classname="MuiSvgIcon-root"/></div>
+       
         
-                <div className="slider">
-                    <div className="left">
+        <div className="wrapper" style={{ left:`-${slide * 100}vw` }}>
+       
+        
+                  { worksData.map((data) =>{
+                       return(
+                        <div className="slider" >
+                        <div className="container" >
+                        <div className="left">
                     
-                    <PhoneAndroidIcon classname="MuiSvgIcon-root"/>
-                    <h1>Title</h1>
-                    <p>Lorem ipsum is placeholder text commonly used in the graphic, 
-                     print, and publishing industries for previewing layouts and visual mockups.
+                    {data.icon}
+                    <h1>{data.title}</h1>
+                    <p>{data.about}</p>
+                    <h3>Projects</h3>
+
+                    
+
+                    </div>
+                    <div className="right">
+
+                    <img src={data.image} alt=""></img>
+                        
+                    </div>
+                    </div>
+                    </div>)
+
+                   })}
+                
+            
+
+       
+           
+        </div>
+        <div className="lArrow" onClick={() =>{handleClick("left")}}><ArrowBackIosIcon classname="MuiSvgIcon-root"/></div>
+        <div className="rArrow" onClick={() =>{handleClick("right")}}><ArrowForwardIosIcon  classname="MuiSvgIcon-root"/></div>     
+        </div>
+    )
+}
